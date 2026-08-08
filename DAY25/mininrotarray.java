942b8cc35167a8345e18744e563f2a6b110c3fa5
Krishn4nmol <anmolkrishna80@gmail.com>
2026-08-08 09:47:35 +0530

    DAY25

---
+++ DAY25/mininrotarray.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class mininrotarray {
+    static int print(int arr[]) { // TC O(logn) SC O(1)
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > arr[high]) {
+                low = mid + 1;   // for max high = mid;
+            }
+            else {
+                high = mid; // for max low = mid + 1;
+            }
+        }
+        return arr[low];    
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) 
+            arr[i] = sc.nextInt();
+        System.out.println(print(arr));
+    }
+}
