59278226e7ae8e12eed487be5af80e16f3b9ba04
Krishn4nmol <anmolkrishna80@gmail.com>
2026-08-21 13:25:48 +0530

    DAY38

---
+++ DAY38/longestpalindrome.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class longestpalindrome {
+    static String find(String s) { // TC O(n^2) SC O(1)
+        if (s.length() <= 1) return s;
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = max(s, i, i);
+            int len2 = max(s, i, i+1);
+            int len = Math.max(len1, len2);
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + (len) / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+    static int max(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(find(s));
+    }
+}
