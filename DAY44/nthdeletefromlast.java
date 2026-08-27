5b797277b50b2077da91460b684648e14b191e1b
Krishn4nmol <anmolkrishna80@gmail.com>
2026-08-27 14:00:45 +0530

    DAY44

---
+++ DAY44/nthdeletefromlast.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class nthdeletefromlast {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    static Node delete(Node head, int n) { // TC O(n) SC O(1)
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node fast = dummy;
+        Node slow = dummy;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Node head = null;
+        Node tail = null;
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            Node newNode = new Node(x);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        int k = sc.nextInt();
+        head = delete(head, k);
+        printList(head);
+    }
+}
