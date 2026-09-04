e63e9f1fb9d63798c740262bf41a81a5da3a5bd8
Krishn4nmol <anmolkrishna80@gmail.com>
2026-09-04 15:40:07 +0530

    DAY52

---
+++ DAY52/subsumk.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class subsumk { // TC O(2^n) SC O(n)
+    static int generate(int arr[], int i, int sum, int k) {
+        if (i == arr.length) {
+            if (sum == k) return 1;
+            return 0;
+        }
+        int take = generate(arr, i + 1, sum + arr[i], k);
+        int nottake = generate(arr, i + 1, sum, k);
+        return take + nottake;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(generate(arr, 0, 0, k))
+    }
+}
