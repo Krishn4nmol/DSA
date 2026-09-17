9bfc56b99c6e61ccc138d7388ca28a7b58ab2b86
Krishn4nmol <anmolkrishna80@gmail.com>
2026-09-17 17:10:45 +0530

    DAY65

---
+++ DAY65/division.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class division {
+    static int divide(int dividend, int divisor) { // TC O(logn*logn) SC O(1)
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        boolean negative = (dividend < 0) ^ (divisor < 0);
+        long dvd = Math.abs((long)dividend);
+        long dvs = Math.abs((long)divisor);
+        long quotient = 0;
+        while (dvd >= dvs) {
+            long temp = dvs;
+            long multiple = 1;
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            dvd -= temp;
+            quotient += multiple;
+        }
+        return negative ? (int) -quotient : (int) quotient;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dividend = sc.nextInt();
+        int divisor = sc.nextInt();
+        System.out.println(divide(dividend, divisor));
+    }
+}
